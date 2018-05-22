699e97c38227e338a277591a2da0e07906e66d11
rv <paul75011@gmail.com>
2018-05-22 04:24:13 +0200

    Making a mistake

--- create.js
+++ create.js
@@ -1,24 +1,25 @@
-import uuid from "uuid";
-import * as dynamoDbLib from "./libs/dynamodb-lib";
-import { success, failure } from "./libs/response-lib";
+import uuid from 'uuid';
+import * as dynamoDbLib from './libs/dynamodb-lib';
+import { success, failure } from './libs/response-lib';
 
 export async function main(event, context, callback) {
-  const data = JSON.parse(event.body);
-  const params = {
-    TableName: process.env.tableName,
-    Item: {
-      userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: uuid.v1(),
-      content: data.content,
-      attachment: data.attachment,
-      createdAt: Date.now()
-    }
-  };
+	gibberish.what;
+	const data = JSON.parse(event.body);
+	const params = {
+		TableName: process.env.tableName,
+		Item: {
+			userId: event.requestContext.identity.cognitoIdentityId,
+			noteId: uuid.v1(),
+			content: data.content,
+			attachment: data.attachment,
+			createdAt: Date.now()
+		}
+	};
 
-  try {
-    await dynamoDbLib.call("put", params);
-    callback(null, success(params.Item));
-  } catch (e) {
-    callback(null, failure({ status: false }));
-  }
+	try {
+		await dynamoDbLib.call('put', params);
+		callback(null, success(params.Item));
+	} catch (e) {
+		callback(null, failure({ status: false }));
+	}
 }
